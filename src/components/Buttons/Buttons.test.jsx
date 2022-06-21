3d733a939b5293d3d24aa66b2886e8bd008c1469
lautaroPH <lautaroph@gmail.com>
2022-06-21 08:34:30 -0300

    add test to buttons

---
+++ src/components/Buttons/Buttons.test.jsx
@@ -0,0 +1,32 @@
+import { fireEvent, render } from '@testing-library/react';
+import { afterEach, describe, expect, test, vi } from 'vitest';
+import GiveAnotherPokemon from './GiveAnotherPokemon';
+import PlayAgain from './PlayAgain';
+
+describe('Buttons', () => {
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  test('should call resetPokemon in giveAnotherPokemon', () => {
+    const mockHandler = vi.fn();
+
+    const { getByText } = render(
+      <GiveAnotherPokemon resetPokemon={mockHandler} />,
+    );
+
+    const button = getByText('Give me another pokemon!');
+    fireEvent.click(button);
+    expect(mockHandler).toHaveBeenCalled(1);
+  });
+
+  test('should call resetPokemon in PlayAgain', () => {
+    const mockHandler = vi.fn();
+
+    const { getByText } = render(<PlayAgain resetPokemon={mockHandler} />);
+
+    const button = getByText('Play again');
+    fireEvent.click(button);
+    expect(mockHandler).toHaveBeenCalled(1);
+  });
+});
